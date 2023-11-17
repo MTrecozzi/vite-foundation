eb9450e02ab71d3b2df73b0910be1a42ebf5a8e7
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2023-11-16 23:01:05 -0500

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,15 +4,33 @@ export default function HomePage() {
   const [count, setCount] = useState<number>(0);
 
   return (
-    <div className="container">
-      <div className="text-3xl">Hello, Vite</div>
-      <button
-        className="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-full"
-        onClick={() => setCount((prev) => prev + 1)}
-      >
-        Increment
-      </button>
-      <div>Clicks: {count}</div>
-    </div>
+    <>
+      {/* Top Bar */}
+      <TopBar />
+      <main className="container mx-auto mt-16">
+        <div className="flex flex-col items-center pt-16">
+          <div className="text-3xl">Hello, Vite</div>
+          <button
+            className="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-full"
+            onClick={() => setCount((prev) => prev + 1)}
+          >
+            Increment
+          </button>
+          <div>Clicks: {count}</div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+function TopBar() {
+  return (
+    <section className="fixed top-0 right-0 left-0" id="topbar">
+      <div className=" flex flex-1 h-16 bg-zinc-100 shadow-xl items-center justify-start px-6 md:px-0">
+        <div className="md:container md:mx-auto">
+          <div className="text-2xl font-bold">Vite Foundation</div>
+        </div>
+      </div>
+    </section>
   );
 }
