271c15d75166177ab1ea56659802c7cf9fccae94
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2023-11-16 22:19:56 -0500

    Successfully Initialized Tailwind with React and Vite

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,10 +4,15 @@ export default function HomePage() {
   const [count, setCount] = useState<number>(0);
 
   return (
-    <body>
-      <div>Hello, Vite</div>
-      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+    <div>
+      <div className="text-3xl">Hello, Vite</div>
+      <button
+        className="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        Increment
+      </button>
       <div>Clicks: {count}</div>
-    </body>
+    </div>
   );
 }
